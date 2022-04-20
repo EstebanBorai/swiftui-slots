0b8a63ca1776dcf0eb1bf7cadd4bc8548c11ea65
Esteban Borai <estebanborai@gmail.com>
2022-04-20 00:13:16 -0400

    feat: use counter and heart states

--- SwiftUILearning/ContentView.swift
+++ SwiftUILearning/ContentView.swift
@@ -9,19 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isHearted = false;
+    @State private var heartCount = 0;
 
     var body: some View {
-        Button(action: {
-            self.isHearted.toggle()
-        }, label: {
-            if isHearted {
-                Image(systemName: "heart.fill")
-                    .foregroundColor(.red)
-            } else {
-                Image(systemName: "heart")
-                    .foregroundColor(.black)
-            }
-        })
+        VStack {
+            Button(action: {
+                self.isHearted.toggle()
+                
+                if self.isHearted {
+                    self.heartCount += 1;
+                }
+            }, label: {
+                if isHearted {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                } else {
+                    Image(systemName: "heart")
+                        .foregroundColor(.black)
+                }
+            })
+            Text(String.init(heartCount))
+        }
     }
 }
 

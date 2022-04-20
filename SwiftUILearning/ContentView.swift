ea2f6dfb33e1103812c0dd4fddca187e74ed6598
Esteban Borai <estebanborai@gmail.com>
2022-04-20 00:10:11 -0400

    feat: state usage with `@State`

--- SwiftUILearning/ContentView.swift
+++ SwiftUILearning/ContentView.swift
@@ -7,49 +7,21 @@
 
 import SwiftUI
 
-var header: some View {
-    HStack(alignment: .center, spacing: 0, content: {
-        Text("Hardcore\nAvo Toast")
-            .fontWeight(.black)
-            .foregroundColor(.white)
-            .font(.headline)
-            .padding(.leading)
-            .frame(width: 200, height: 50, alignment: .leading)
-        Spacer()
-        Image("HeartHealth")
-            .padding(.trailing)
-            .frame(width: 20.0, height: 20.0, alignment: .trailing)
-    })
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-    .background(Image("TopGradient"))
-}
-
-var footer: some View {
-    HStack(alignment: .center, spacing: 0, content: {
-        Text("Energize with this healthy and hearty breakfast")
-            .font(.footnote)
-            .fontWeight(.semibold)
-            .foregroundColor(.white)
-    })
-    .padding(.bottom)
-    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-    .background(Image("BottomGradient"))
-}
-
-var appBackground: some View {
-    Image("Breakfast")
-        .resizable()
-        .ignoresSafeArea(.all)
-}
-
 struct ContentView: View {
+    @State private var isHearted = false;
+
     var body: some View {
-        VStack {
-            header
-            Spacer()
-            footer
-        }.background(Image("Breakfast"))
-        .frame(width: .infinity, height: .infinity, alignment: .center)
+        Button(action: {
+            self.isHearted.toggle()
+        }, label: {
+            if isHearted {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+            } else {
+                Image(systemName: "heart")
+                    .foregroundColor(.black)
+            }
+        })
     }
 }
 
